292c22b62c67ae52deca634cd2dfbed9f502aed3
geon-2 <gunle0416@gmail.com>
2026-05-09 15:26:49 +0900

    feat: proxy 설정 추가

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,13 @@ import process from 'node:process'
 export default defineConfig(() => {
   return {
     plugins: [react()],
+    proxy: {
+      '/api': {
+        target: process.env.API_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
     define: {
       'process.env.S3_BUCKET': JSON.stringify(process.env.S3_BUCKET || ''),
       'process.env.AWS_REGION': JSON.stringify(process.env.AWS_REGION || ''),
